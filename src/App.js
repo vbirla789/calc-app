1b41b4c6119724689335c4dd910137901e924a05
VIIBII <birlavishal90@gmail.com>
2023-02-10 18:10:54 +0530

    calc app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./index.css"
 
 function App() {
+
+  const dig =() => {
+    const digArr =[]
+
+    for( let i=0; i<10; i++){
+      digArr.push 
+        (<button key={i} onClick={() => updateCalc(i.toString())}> {i} </button>
+      )}
+    return digArr
+  }
+
+const[calc, setCalc]=useState('')
+const[result, setResult]=useState('')
+
+const ops =['/', '*', '+', '-', '.']
+
+const updateCalc = value => {
+
+  if (ops.includes(value) && calc === ''||
+  ops.includes(value) && ops.includes(calc.slice(-1))){
+    return
+  }
+
+  setCalc(calc + value)
+
+  if(!ops.includes(value)){
+    setResult(eval(calc + value))
+  }
+  
+}
+
+const calculate =()=>{
+  setCalc(eval(calc).toString())
+}
+const del=()=>{
+  if(!calc ===""){
+    return
+  }
+  setCalc('')
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc">
+
+        <div className="display">
+          { calc || ''} { result ? <span>({result})</span> : ''}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc('/')} >/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+        </div>
+
+        <div className="digits">
+          {dig()}
+          <button onClick={() => updateCalc('.')}>.</button>
+          <button onClick={del}>del</button>
+          <button onClick={calculate}>=</button>
+        </div>
+
+      </div>
     </div>
   );
 }
